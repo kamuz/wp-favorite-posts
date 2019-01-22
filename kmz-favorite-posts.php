8e347fd72fead95369c3d26890857221bc4fc99c
Vladimir Kamuz <v.kamuz@gmail.com>
2019-01-22 15:56:49 +0200

    File headers

---
+++ kmz-favorite-posts.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Plugin Name: KMZ Favorite Posts
+ * Description: Display and manage favorite posts
+ * Version: 1.0.0
+ * Author: Vladimir Kamuz
+ * License: GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ * Author URI: https://wpdev.pp.ua/
+ * Plugin URI: https://github.com/kamuz/wp-favorite-posts
+ * Text Domain: kmz-favorite-posts
+ * Domain Path: /languages
+*/
